0ca6ab011eb1f94b2a7f11e529289d4642327278
Robel-S <robelsebhat2@gmail.com>
2025-06-08 19:23:27 -0700

    added folders and stylesheet

--- main/todo_list.py
+++ main/todo_list.py
@@ -1,10 +1,12 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtGui import QFont
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.setGeometry(700,300,1000, 600)
+        
 def main():
     app = QApplication([])
     window = Widget()
